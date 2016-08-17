fd4e8e287bc1c70cc7953b2fb9c86ce9069ee249
Ievgen <ievgenbazilevych@gmail.com>
2016-08-17 16:44:54 -0400

    17/08

---
+++ src/main/java/smoke/test/BaseTest.java
@@ -0,0 +1,42 @@
+package smoke.test;
+
+import com.codeborne.selenide.Screenshots;
+import com.google.common.io.Files;
+import com.sun.xml.internal.ws.dump.LoggingDumpTube;
+import org.junit.After;
+import org.junit.Before;
+import ru.yandex.qatools.allure.annotations.Attachment;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
+
+
+/**
+ * Created by barocko on 8/17/2016.
+ */
+public class BaseTest {
+
+    @After
+    public void tearDown() throws IOException {
+        File lastSelenideScreenshot = Screenshots.getLastScreenshot();
+        if (lastSelenideScreenshot != null) {
+            screenshot(Files.toByteArray(lastSelenideScreenshot));
+        }
+    }
+
+    @Attachment(type = "image/png")
+    public static byte[] screenshot(byte[] dataForScreenshot) {
+        return dataForScreenshot;
+    }
+
+    @Before
+    public void openPage() {
+        open("https://google.com/ncr");
+    }
+
+}
+
+
---
+++ src/main/java/smoke/test/GoogleSearchTest.java
@@ -0,0 +1,37 @@
+package smoke.test;
+
+import com.codeborne.selenide.ElementsCollection;
+import org.junit.Test;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+/**
+ * Created by barocko on 8/17/2016.
+ */
+public class GoogleSearchTest {
+
+    @Test
+    public void testSearch() {
+
+        add("“Selenium automates browsers”");
+
+        assertResultsListed(10);
+    }
+
+    ElementsCollection searchResults = $$("#lst-ib");
+
+    @Step
+    private void add(String... taskTexts) {
+        for (String text : taskTexts) {
+            $("#lst-ib").setValue(text).pressEnter();
+        }
+    }
+
+    public void assertResultsListed(int count) {
+        searchResults.shouldHave(size(count));
+    }
+
+}
