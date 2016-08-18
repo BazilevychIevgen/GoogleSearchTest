637d3410c3f5d5e4def47e011554bc2168e40317
Ievgen <ievgenbazilevych@gmail.com>
2016-08-18 14:59:00 -0400

    17/08

--- src/main/java/smoke/test/BaseTest.java
+++ src/main/java/smoke/test/BaseTest.java
@@ -5,6 +5,7 @@ import com.google.common.io.Files;
 import com.sun.xml.internal.ws.dump.LoggingDumpTube;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import ru.yandex.qatools.allure.annotations.Attachment;
 
 import java.io.File;
@@ -31,7 +32,6 @@ public class BaseTest {
     public static byte[] screenshot(byte[] dataForScreenshot) {
         return dataForScreenshot;
     }
-
 
 }
 
--- src/main/java/smoke/test/GoogleSearchTest.java
+++ src/main/java/smoke/test/GoogleSearchTest.java
@@ -1,19 +1,24 @@
 package smoke.test;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import ru.yandex.qatools.allure.annotations.Step;
 
 import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.url;
 
 /**
  * Created by barocko on 8/17/2016.
  */
-public class GoogleSearchTest {
+public class GoogleSearchTest extends BaseTest {
 
     @Test
     public void testSearch() {
@@ -22,26 +27,36 @@ public class GoogleSearchTest {
 
         search("“Selenium automates browsers”");
 
-        assertResultsListed(10);
+        assertResultsCount(10);
+
+        followLink(0);
+
+        pageIsDownloaded();
     }
 
-    ElementsCollection searchResults = $$("#lst-ib");
+    ElementsCollection results = $$(".srg>.g");
 
     @Step
-    private void search(String... taskTexts) {
-        for (String text : taskTexts) {
-            $("#lst-ib").setValue(text).pressEnter();
-        }
+    public void search(String text) {
+        $(byName("q")).setValue(text).pressEnter();
     }
 
-    public void assertResultsListed(int count) {
-        searchResults.shouldHave(size(count));
+    public void assertResultsCount(int count) {
+        results.shouldHave(size(count));
     }
 
+    public void followLink(int index) {
+        results.get(index).find("h3>a").click();
+    }
 
+    public void pageIsDownloaded() {
+        $("#editPage>a").shouldBe(visible);
+        Assert.assertEquals("http://www.seleniumhq.org/", url());
+    }
 
-    public void openPage() {
+    public static void openPage() {
         open("https://google.com");
     }
+
 
 }
